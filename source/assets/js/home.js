cacfa01318476887ee9a55b73109eb778eb2082a
Fabricio Lucas <107938164+fabriciolucasz@users.noreply.github.com>
2024-05-07 19:23:40 -0300

    Removing trash

--- source/assets/js/home.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const isLogged = "Usuário"
-    const notLogged = "Visitante" ?? isLogged
-    const text = document.getElementById("welcome_text")
-    text.innerText = text.innerText.replace("${visitant}", notLogged)
-    console.log(text)
-}
